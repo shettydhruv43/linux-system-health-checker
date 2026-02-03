eb0df19939110f3f507ed431608954891dc4731d
dhruv <dhruv.shetty@student.ufv.ca>
2026-02-02 17:00:33 -0800

    Add disk usage thresholds, exit codes, and ignore generated files

--- health_check.sh
+++ health_check.sh
@@ -1,27 +1,65 @@
 #!/bin/bash
 
+# ======================================
+# Linux System Health Checker Script
+# Author: Dhruv Shetty
+# Description:
+# This script collects system health information
+# like uptime, memory usage, disk usage,
+# CPU load, and logged-in users.
+# ======================================
+
+# Create a timestamped report file
+REPORT="health_report_$(date +%F_%H-%M-%S).txt"
+WARN_DISK=80
+CRIT_DISK=90
+EXIT_STATUS=0
+
 echo "======================================"
 echo "     Linux System Health Report"
 echo "======================================"
 echo "Generated on: $(date)"
 echo ""
 
-echo "----- SYSTEM UPTIME -----"
-uptime
+# Check system uptime information
+echo "----- SYSTEM UPTIME -----" >> $REPORT
+uptime >> $REPORT
 echo ""
 
-echo "----- MEMORY USAGE -----"
-free -h
+# Chcek memory usage
+echo "----- MEMORY USAGE -----" >> $REPORT
+free -h >> $REPORT
 echo ""
 
-echo "----- DISK USAGE -----"
-df -h
+# Check disk usage of mounted filesystems (with thresholds)
+echo "----- DISK USAGE -----" >> $REPORT
+df -P -h | awk 'NR==1 || $1 ~ "^/dev"' >> $REPORT
+echo "" >> $REPORT
+
+# Disk threshold checks
+while read -r fs size used avail use mount; do
+  usage=${use%\%}
+
+  if (( usage >= CRIT_DISK )); then
+    echo "CRITICAL: $mount is at ${usage}% used" >> $REPORT
+    EXIT_STATUS=2
+  elif (( usage >= WARN_DISK )) && (( EXIT_STATUS < 2 )); then
+    echo "WARNING: $mount is at ${usage}% used" >> $REPORT
+    EXIT_STATUS=1
+  fi
+done < <(df -P -h | awk 'NR>1 && $1 ~ "^/dev"')
+
+echo "" >> $REPORT
+
+
+# Capture CPU load and running processes
+echo "----- CPU LOAD -----" >> $REPORT
+top -bn1 | head -n 5 >> $REPORT
 echo ""
 
-echo "----- CPU LOAD -----"
-top -bn1 | head -n 5
+# List currently logged-in users
+echo "----- LOGGED-IN USERS -----" >> $REPORT
+who >> $REPORT
 echo ""
 
-echo "----- LOGGED-IN USERS -----"
-who
-echo ""
+exit $EXIT_STATUS
